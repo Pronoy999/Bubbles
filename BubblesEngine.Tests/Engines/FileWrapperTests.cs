913eec855b45058f1d02433f7ae78da46d8e2af8
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2021-08-27 16:05:01 +0530

    Add File Wrapper tests.

--- BubblesEngine.Tests/Engines/FileWrapperTests.cs
+++ BubblesEngine.Tests/Engines/FileWrapperTests.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using BubblesEngine.Engines;
 using BubblesEngine.Engines.Implementations;
 using Moq;
@@ -22,6 +21,55 @@ namespace BubblesEngine.Tests.Engines
         {
             _domainFs.Setup(fs => fs.WriteFile(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
             var result = await _fileWrapper.CreateFile("some-path", "some-content");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void ShouldNotCreateAFileWithInvalidPath()
+        {
+            _domainFs.Setup(fs => fs.WriteFile(null, It.IsAny<string>())).ReturnsAsync(false);
+            var result = await _fileWrapper.CreateFile(null, null);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async void ShouldReadContentsOfValidFile()
+        {
+            _domainFs.Setup(fs => fs.ReadFile(It.IsAny<string>())).ReturnsAsync("some-content");
+            var result = await _fileWrapper.GetFileContents("some-path");
+            Assert.Equal("some-content", result);
+        }
+
+        [Fact]
+        public void ShouldCheckWhenAValidFileExists()
+        {
+            _domainFs.Setup(fs => fs.IsExists(It.IsAny<string>())).Returns(true);
+            var result = _fileWrapper.IsExists("some-path");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldCheckWhenAnInvalidFilePathIsPassed()
+        {
+            _domainFs.Setup(fs => fs.IsExists(null)).Returns(false);
+            var result = _fileWrapper.IsExists(null);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldCreateADirectoryWhenValidPathIsPassed()
+        {
+            _domainFs.Setup(fs => fs.CreateDirectory(It.IsAny<string>())).Returns(true);
+            var result = _fileWrapper.CreateFolder("some-path");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldNotCreateADuplicateFolder()
+        {
+            _domainFs.Setup(fs => fs.IsExists(It.IsAny<string>())).Returns(true);
+            _domainFs.Setup(fs => fs.CreateDirectory(It.IsAny<string>())).Returns(true);
+            var result = _fileWrapper.CreateFolder("some-path");
             Assert.True(result);
         }
     }
